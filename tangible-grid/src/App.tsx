8fc79dcaef9037f06ad212d38325e120c72b3597
Arnav Mittal <arnav.mittal09@gmail.com>
2024-03-13 13:40:11 -0400

    Added TS React app

---
+++ tangible-grid/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    
+
+    return (
+        <div className="App">
+            <div id="toolbar">
+            </div>
+            <div id="container"></div>
+        </div>
+    );
+}
+
+export default App;
